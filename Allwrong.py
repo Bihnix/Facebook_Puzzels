42e7e1e87016beccccf68ddee9276f01dea2ba8d
Behnam Ebrahimi <48855903+Bihnix@users.noreply.github.com>
2022-02-07 18:18:19 +0000

    Update Allwrong.py

--- Allwrong.py
+++ Allwrong.py
@@ -1,12 +1,1 @@
-# Write any import statements here
 
-def getWrongAnswers(N: int, C: str) -> str:
-  # Write your code here
-  ans=[]
-  for char in C:
-    if char == "A":
-      ans.append(char.replace("A","B"))
-    elif char != "A":
-      ans.append(char.replace("B","A"))
-
-  return ''.join(ans)
