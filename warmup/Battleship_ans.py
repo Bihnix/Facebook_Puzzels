3083397f07cb97fd26f041b37d06d1891d2b8715
Behnam Ebrahimi <48855903+Bihnix@users.noreply.github.com>
2022-02-07 18:41:10 +0000

    Update Battleship_ans.py

--- warmup/Battleship_ans.py
+++ warmup/Battleship_ans.py
@@ -1,7 +1,8 @@
-
 from typing import List
 # Write any import statements here
-
+import numpy as np
 def getHitProbability(R: int, C: int, G: List[List[int]]) -> float:
   # Write your code here
-  return 0.0
+  A = R *  C
+  p_value = (np.sum(G)/A)
+  return p_value
